776259d23536ae484e234d94454e5d7a9aae8f7c
rub1e <alex.rubner@gmail.com>
2015-06-03 13:07:46 +0100

    ffs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var warbleArray = [];
 
 function Warble(content) {
-	this.wId = ""; // length of master array + 1?????
 	this.content = content;
 	this.timeStamp = new Date().getTime();
 	this.user = ""; // cookie ID????
@@ -48,19 +47,19 @@ $("#warbleSubmit").click(function(){
 	})
 })
 
-console.log('script loaded');
-var listWarbles = document.getElementById('listWarbles');
-//load list of warbles
-var request = new XMLHttpRequest();
-request.open('GET', '/warbles');
-request.send(null);
-request.onreadystatechange = function(){
-  if(request.readyState === 4 && request.status === 200){
-    var list = JSON.parse(request.responseText);
-    var htmlList = '';
-    list.forEach(function(element){
-      htmlList += "<li>" + element.content + "</li>";
-    });
-    listWarbles.innerHTML = htmlList;
-  }
-};
+// console.log('script loaded');
+// var listWarbles = document.getElementById('listWarbles');
+// //load list of warbles
+// var request = new XMLHttpRequest();
+// request.open('GET', '/warbles');
+// request.send(null);
+// request.onreadystatechange = function(){
+//   if(request.readyState === 4 && request.status === 200){
+//     var list = JSON.parse(request.responseText);
+//     var htmlList = '';
+//     list.forEach(function(element){
+//       htmlList += "<li>" + element.content + "</li>";
+//     });
+//     listWarbles.innerHTML = htmlList;
+//   }
+// };
