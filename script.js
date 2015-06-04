ea3b49d04ab957a6ed8f64a630fefc2bb0365f87
rub1e <alex.rubner@gmail.com>
2015-06-04 15:49:00 +0100

    fixed warblers

--- script.js
+++ script.js
@@ -76,19 +76,3 @@ $("#warbleSubmit").click(function(){
 	});
 });
 
-// console.log('script loaded');
-// var listWarbles = document.getElementById('listWarbles');
-// //load list of warbles
-// var request = new XMLHttpRequest();
-// request.open('GET', '/warbles');
-// request.send(null);
-// request.onreadystatechange = function(){
-//   if(request.readyState === 4 && request.status === 200){
-//     var list = JSON.parse(request.responseText);
-//     var htmlList = '';
-//     list.forEach(function(element){
-//       htmlList += "<li>" + element.content + "</li>";
-//     });
-//     listWarbles.innerHTML = htmlList;
-//   }
-// };
