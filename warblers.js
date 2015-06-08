e3393031272b064a741c5b9e7fb66d0966ddaa62
SimonLab <simon.labondance@gmail.com>
2015-06-08 15:15:04 +0100

    add config heroku

--- warblers.js
+++ warblers.js
@@ -5,7 +5,7 @@ var redis = require('redis');
 // var client = redis.createClient();
 
 var url = require('url');
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var redisURL = url.parse(process.env.REDIS_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 client.auth(redisURL.auth.split(":")[1]);
 
