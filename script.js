3505c8ee467ec033267b8d8d6ae58f810d5e30aa
Anita <a.nita@hotmail.co.uk>
2015-06-10 17:35:53 +0100

    adding jsonfile fetch -atm console.log location not attached to warble

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ $('#warbleForm').submit(function(e){
 	// 	warble.latitude = position.coords.latitude;
 	// 	warble.longitude = position.coords.longitude;
 	// });
+	fetchJSONFile('http://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat +'&lon=' + long+ '&zoom=18&addressdetails=1', function(data) {
+       console.log(data);
+        var location = data.address.city;
+        console.log(location);
+        // document.getElementById("output").innerHTML = data;
+        // do something with your data
+    });
 	var warbleString = JSON.stringify(warble);
 	socket.emit('warble', warbleString);
 	leaflet.createMarker(warble);
@@ -25,6 +32,32 @@ $('#warbleForm').submit(function(e){
 	}
 	return false;
 });
+
+function fetchJSONFile(path, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback) {callback(data)}
+                }
+            }
+        };
+        httpRequest.open('GET', path);
+        httpRequest.send();
+    }
+    // this requests the file and executes a callback with the parsed result once
+    //   it is available
+    var long = 0.0423496;
+    var lat = 51.529527099999996;
+    fetchJSONFile('http://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat +'&lon=' + long+ '&zoom=18&addressdetails=1', function(data) {
+       console.log(data);
+        var location = data.address.city;
+        // console.log(location);
+        // document.getElementById("output").innerHTML = data;
+        // do something with your data
+    });
+
 
 socket.on('warble', function(data){
 	var warble = JSON.parse(data);
@@ -47,7 +80,7 @@ function addWarble(data) {
 	var unWarble = data.user === localStorage.getItem("warblerBrowserID") ? "<input type='button' class='unwarble' value='UnWarble'>" : "";
 	return "<li class='warble'>" + data.content + 
 	"<br/><span class='date' id='" + data.timestamp + "'>Warbled at " + 
-	new Date(data.timestamp).toString().slice(0, 24) + " Located at: " + data.latitude + ", " + data.longitude +
+	new Date(data.timestamp).toString().slice(0, 24) + " Located at: " + data.latitude + ", " + data.longitude + 
 	"</span>" + unWarble + "</li>";
 }
 
